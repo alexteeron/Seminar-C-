7040bfb1969e5d9f8d6eb17c7bfcd32676e06e99
Alexandr Kim <corpicorns@yandex.ru>
2023-03-25 00:26:03 +0300

    sem1Tesk00

---
+++ Sem1Tesk00/Program.cs
@@ -0,0 +1,18 @@
+﻿// Считываем данные с консоли
+String? inLine = Console.ReadLine();
+
+// Проверяем чтоб данные не были пустыми
+if (inLine != null)
+{
+    //Парсим введеное число
+    int inNumber = int.Parse(inLine);
+    // другой вариант возведение в квадрат
+    int OutStrPow = (int)Math.Pow(inNumber, 2);
+
+    // Находим квадрат числа
+    int outNamber = inNumber * inNumber;
+
+    // Ввыводим данные в консоль
+    Console.WriteLine(outNamber);
+    Console.WriteLine(OutStrPow);
+}
