bbc69f905108261213fd568feba5a718ae2394a3
Alexandr Kim <corpicorns@yandex.ru>
2023-03-25 20:55:28 +0300

    5,7 на выходе получили 7 = max

--- Sem1Tesk002/Program.cs
+++ Sem1Tesk002/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число");
+﻿//Программа для выисления максимального и минимального значения из 2х чисел
+//задание: ввести числа 5,7; 2,10; -9,-3
+Console.WriteLine("Введите число");
+//Сконвертируем значение консоли
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -9,5 +12,5 @@ if (a > b) max =a;
            min =b;
 if(b>a) max = b; 
         min = a;
-Console.WriteLine(min);
-Console.WriteLine(max);
+Console.WriteLine(min); //Получаем минимальное значение
+Console.WriteLine(max); //получаем максимальное значение
