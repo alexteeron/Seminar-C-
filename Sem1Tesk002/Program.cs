377b8d20ff6c82ee003a7a7a4d9ccc079d8b1a62
Alexandr Kim <corpicorns@yandex.ru>
2023-03-25 20:05:58 +0300

    Sem1Task002

---
+++ Sem1Tesk002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+if (a > b) max =a;
+           min =b;
+if(b>a) max = b; 
+        min = a;
+Console.WriteLine(min);
+Console.WriteLine(max);
