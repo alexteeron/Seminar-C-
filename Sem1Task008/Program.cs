b4fafab416d35815c2c726b509f6dabc375d3ee1
Alexandr Kim <corpicorns@yandex.ru>
2023-03-26 18:09:01 +0300

    Вычисление всех чётных от 0 до N значения

---
+++ Sem1Task008/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+String? InLine = Console.ReadLine();
+
+if (InLine != null)
+{
+    int x = 0;
+    int count = 1;
+    int N = int.Parse(InLine);
+    while (count <= N)
+    {
+        if (count % 2 == 0) 
+        {
+            x = x + 2; 
+            Console.Write(x + ", ");
+        }
+        count++;
+    }
+}
