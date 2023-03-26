747182dd8493807e7cad07738a2e1106a872a970
Alexandr Kim <corpicorns@yandex.ru>
2023-03-26 16:21:16 +0300

    задание на деление без остатка

---
+++ Sem1Task006/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.WriteLine("Введите число");
+string? InLine = Console.ReadLine();
+
+
+if (InLine != null)
+{
+
+    int N = int.Parse(InLine);
+
+    if (N % 2 == 0)
+    {
+        Console.WriteLine("чётное");
+    }
+
+    else
+    {
+        Console.WriteLine("не чётное");
+    }
+
+}
+
